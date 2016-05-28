1244e21359a90c53d9b63c7841c8e3609b82ffb7
Stéphane BAUSSERON <stephane.bausseron@gmail.com>
2016-05-28 20:29:31 +0200

    [Unittest] Using strict mode on unit tests

--- unittest.sh
+++ unittest.sh
@@ -46,25 +46,25 @@ expect_nok() {
 }
 
 rm -f unittest.log
-expect_ok "Python correctly formed" "$CB check tests/python_file.py"
+expect_ok "Python correctly formed" "$CB check -S tests/python_file.py"
 
-expect_nok "Python malformed" "$CB check tests/python_file_fail.py"
+expect_nok "Python malformed" "$CB check -S tests/python_file_fail.py"
 cp tests/python_file_fail.py tests/python_file_fixed.py
 expect_ok "Python, format malformed source" "$CB format tests/python_file_fixed.py"
-expect_ok "Python, check fixed source" "$CB check tests/python_file_fixed.py"
+expect_ok "Python, check fixed source" "$CB check -S tests/python_file_fixed.py"
 
-expect_ok "Python, shebang correctly formed 1" "$CB check tests/python_shebang_ok_1"
-expect_ok "Python, shebang correctly formed 2" "$CB check tests/python_shebang_ok_2"
-expect_ok "Python, shebang correctly formed 3" "$CB check tests/python_shebang_ok_3"
+expect_ok "Python, shebang correctly formed 1" "$CB check -S tests/python_shebang_ok_1"
+expect_ok "Python, shebang correctly formed 2" "$CB check -S tests/python_shebang_ok_2"
+expect_ok "Python, shebang correctly formed 3" "$CB check -S tests/python_shebang_ok_3"
 
 # Expect ok but with a warning about file format which is not recognized. For now we just check the exit code.
-expect_ok "Python, shebang malformed 1" "$CB check tests/python_shebang_fail_1"
-expect_ok "Python, shebang malformed 2" "$CB check tests/python_shebang_fail_2"
-expect_ok "Python, shebang malformed 3" "$CB check tests/python_shebang_fail_3"
+expect_nok "Python, shebang malformed 1" "$CB check -S tests/python_shebang_fail_1"
+expect_nok "Python, shebang malformed 2" "$CB check -S tests/python_shebang_fail_2"
+expect_nok "Python, shebang malformed 3" "$CB check -S tests/python_shebang_fail_3"
 
-expect_ok "C++ correctly formed" "$CB check tests/file.cpp"
-expect_nok "C++ malformed" "$CB check tests/file_fail_1.cpp"
-cp tests/file_fail_1.cpp tests/file_fail_1.cpp
-expect_ok "Format C++ malformed" "$CB format tests/file_fail_1.cpp"
-expect_ok "Check C++ formatted" "$CB check tests/file_fail_1.cpp"
+expect_ok "C++ correctly formed" "$CB check -S tests/file.cpp"
+expect_nok "C++ malformed" "$CB check -S tests/file_fail_1.cpp"
+cp tests/file_fail_1.cpp tests/file_fixed_1.cpp
+expect_ok "Format C++ malformed" "$CB format tests/file_fixed_1.cpp"
+expect_ok "Check C++ formatted" "$CB check -S tests/file_fixed_1.cpp"
 
